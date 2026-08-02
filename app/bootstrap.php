9221faf437bea18d29f27a78b509671a11bbdce0
Dav-Olobo <davidolobo10@gmail.com>
2026-08-02 23:20:33 +0100

    Load .env only when present

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,19 @@
 <?php
 
+// For Render Platform deployment, you can use the following SMTP settings for testing email sending:
+// - SMTP Host: smtp.mailtrap.io  
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+}
+
+// For localhost development, you can use the following SMTP settings for testing email sending:
+// - SMTP Host: sandbox.smtp.mailtrap.io
+
+// require_once __DIR__ . '/../vendor/autoload.php';
+
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+// $dotenv->load();
